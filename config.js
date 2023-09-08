03d4d17b83bc3e21d150aa46beac3e1e1fa69983
Azkaazhrzlva <143519385+Azkaazhrzlva@users.noreply.github.com>
2023-09-09 03:04:12 +0700

    Update config.js

--- config.js
+++ config.js
@@ -7,13 +7,13 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['919074692450', 'ABHISHEK-SURESH', true],
+  ['6283830802015', 'OnlyAzka', true],
   [''],
   [''] 
 ] //Number of owners
 
-global.mods = ['919074692450','919074692450'] 
-global.prems = ['919074692450', '919074692450', '919074692450']
+global.mods = ['6283830802015','919074692450'] 
+global.prems = ['6283830802015', '919074692450', '919074692450']
 global.keysZens = ['c2459db922', '37CC845916', '6fb0eff124']
 global.keysxxx = keysZens[Math.floor(keysZens.length * Math.random())]
 global.keysxteammm = ['29d4b59a4aa687ca', '5LTV57azwaid7dXfz5fzJu', 'cb15ed422c71a2fb', '5bd33b276d41d6b4', 'HIRO', 'kurrxd09', 'ebb6251cc00f9c63']
@@ -47,13 +47,13 @@ global.APIKeys = { // APIKey Here
 }
 
 // Sticker WM
-global.botname = '𝙰𝙱𝙷𝙸𝚂𝙷𝙴𝙺-𝚂𝙴𝚁'
+global.botname = '𝙰𝚣𝚔𝚊-𝙼𝙳'
 global.premium = 'true'
-global.packname = '𝙰𝙱𝙷𝙸𝚂𝙷𝙴𝙺-𝚂𝙴𝚁🎯𝙰𝙱𝙷𝙸𝚂𝙷𝙴𝙺-𝚂𝚄𝚁𝙴𝚂𝙷☘️' 
-global.author = 'ABHISHEK SURESH' 
-global.igfg = '▢ Follow on Instagram\nhttps://www.instagram.com/abhishek_ser\n' 
-global.dygp = 'https://chat.whatsapp.com/BOLb0ICN3sAJ5dloRBw5VD'
-global.fgsc = 'https://github.com/AbhishekSuresh2/ABHISHEK-SER' 
+global.packname = '𝙰𝚣𝚔𝚊-𝙼𝙳𓊈+𝟼𝟸-𝟾𝟹𝟾-𝟹𝟶𝟾𝟶-𝟸𝟶𝟷𝟻𓊉' 
+global.author = '𝙾𝚗𝚕𝚢𝙰𝚣𝚔𝚊' 
+global.igfg = '▢ Follow on Instagram\nhttps://www.instagram.com/𝙰𝚣𝚔𝚊𝚊𝚣𝚑𝚛𝚣𝚕𝚟𝚊\n' 
+global.dygp = 'https://chat.whatsapp.com/KKo8krcS1cn7eLMGt7nOo4'
+global.fgsc = 'https://github.com/𝙰𝚣𝚔𝚊𝚊𝚣𝚑𝚛𝚣𝚕𝚟𝚊/ABHISHEK-SER' 
 global.fgyt = 'https://youtube.com/@comedymelodych8468'
 global.fgpyp = 'https://youtube.com/@comedymelodych8468'
 global.fglog = 'https://replicate.delivery/pbxt/QbP6Fh3ZXwKON9SCB70ERGwwgeeSbztwKIOIzhUeXFkwnFHiA/out.png' 
